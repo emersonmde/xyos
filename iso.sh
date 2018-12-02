6d59d2e1ce72c6c03514473109c6c06ab202869c
Matthew Emerson <emersonmde@protonmail.com>
2018-12-02 00:15:22 -0500

    Updated kernel binary name

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/xyos isodir/boot/xyos
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "XyOS" {
+	multiboot /boot/xyos
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o xyos.iso isodir
